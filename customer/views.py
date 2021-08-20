3e4f4076bb5975fa2106e6cd620b55d1b975011e
Alesalg <allesalg@gmail.com>
2021-08-20 11:57:41 -0300

    add regex and fix path

--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, CreateView
 from .models import Customer
 from .forms import CustomerForm
+from django.urls import reverse
 
 class CustomerListView(ListView):
     template_name = "customer/customer_list.html"
@@ -13,3 +14,6 @@ class CustomerCreateView(CreateView):
 
     def form_valid(self, form):
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse ("customer:customer-list")
